f2c0985ff4b9b43b8a39252425aa81f162b3afa9
Vitali Pareika <vitali.pareika@gmail.com>
2021-11-11 18:59:53 +0100

    Initial commit

---
+++ src/main/java/com/example/DropwizardApplication.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class DropwizardApplication extends Application<DropwizardConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new DropwizardApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "Dropwizard";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<DropwizardConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final DropwizardConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+    }
+
+}
